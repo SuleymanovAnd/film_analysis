521dc2275ef1c26a35602cb9d7a1d647d66c924a
SuleymanovAnd <106871724+SuleymanovAnd@users.noreply.github.com>
2022-12-17 20:07:56 +0200

    Update actor_search.cpp

--- src/actor_search.cpp
+++ src/actor_search.cpp
@@ -2,7 +2,7 @@
 struct Film {
     std::string name;
     std::string country;
-    int date;
+    string date;
     std::string studio;
     std::string author;
     std::string director;
@@ -14,6 +14,18 @@ struct Film {
 std::string actor_search (std::string name, std::string surname, nlohmann::json films){
    Film newFilm;
    std::string actor = name + surname;
+    
+    nlohmann::json::iterator it;
+    
+    for (it = films.begin(); it != films.end();++it){
+      newFilm.name = *it["name"];
+      newFilm.country = *it["country"];
+      newFilm.date = *it["date"];
+      newFilm.studio = *it["studio"];
+      newFilm.director = *it["director"];
+      newFilm.producer = *it["producer"];
+      
+    }
 
    newFilm.characters
 
